afd6808acc6affbbc9871a4dbe656b3a5f15c866
Isaac Shabtay <isaac-solvo@users.noreply.github.com>
2022-09-28 11:25:33 -0700

    Added print

--- resources/action.py
+++ resources/action.py
@@ -1,8 +1,13 @@
 import argparse
 import os
 import sys
+import json
+import logging
 
 import requests
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO, format="[%(levelname)-8s] %(message)s")
+logger = logging.getLogger(__name__)
 
 
 def main():
@@ -12,7 +17,7 @@ def main():
     pr_title = args.pr_title
 
     words = pr_title.split(":", 1)
-    issue_key = words[0]
+    issue_key = words[0].strip()
 
     jira_url = os.getenv("JIRA_URL")
     if not jira_url:
@@ -24,6 +29,8 @@ def main():
     if not jira_api_token:
         raise Exception("Missing JIRA TOKEN key")
 
+    logger.info(f"Searching for issue '{issue_key}' in JIRA server located at {jira_url}")
+
     response = requests.get(
         f"{jira_url}/rest/api/latest/issue/{issue_key}",
         auth=(jira_email, jira_api_token))
@@ -33,6 +40,14 @@ def main():
 
     if response.status_code != 200:
         raise Exception(f"Unexpected response code: {response.status_code} (text: {response.text})")
+
+    try:
+        issue_title = response.json()["fields"]["summary"]
+    except Exception:
+        logger.warning(f"Failed retrieving summary of issue {issue_key}", exc_info=True)
+        issue_title = "N/A"
+
+    logger.info(f"Found issue {issue_key}: {issue_title}")
 
 
 if __name__ == "__main__":
